864380cdd241e017d64f43fa7301ed09d3db1f24
belhard-user <ldarik2l@gmail.com>
2018-02-22 20:42:35 +0200

    jquery

--- assets/index.js
+++ assets/index.js
@@ -1,65 +1,41 @@
 import $ from 'jquery';
 
-// $('ul li')
-// $('ul').children().text('Hey ;)');
-
-document.querySelector('button').addEventListener('click', () => {
-    // let attr = $('input').attr('type');
-    $('input').attr({
-        id: 'foo',
-        class: 'bar',
-        'data-fignja': 1111
-    });
 
 
-    /*console.log(
-        $('input').val('asdasd')
-    );*/
+$('ul').on('click', 'li', function (e) {
+    alert($(this).text());
 });
 
 
+$('form:first').on('submit', function(e){
+    e.preventDefault();
+    let input = $(this).find('input');
 
-/*$('ul li').html (`
-    <h1>Title</h1>
-    <p>lorem</p>
-    <p>ipsum</p>
-    <p>dolor</p>
-    <h2>dolor</h2>
-`);*/
+    $('<li>', {
+        text: input.val()
+    }).appendTo('ul:first');
 
-/*$('ul li').text(function(index, text){
-    return `Hello world ${index}`;
-});*/
+    input.val('');
+});
 
+document.querySelector('button').addEventListener('click', () => {
+    // let str = 'Hey';
+    // let className = 'foo';
+    //
+    // let li = $('<li>', {
+    //     html: '<strong>some text</strong>',
+    //     class: 'asdadsasd',
+    //     id: 'asdasd'
+    // });
 
+    //li.css('color', 'gold');
 
+    // li.insertAfter('li');
+    // $('li').eq(3).after(li);
 
+    // li.prependTo('ul:first');
+    //$('ul:first').prepend(li);
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*let container = $('#container');
-let section = $('section.bar');*/
-// let firstChildInsideSection = $('#container p:first-child');
-// let firstChildInsideSection = $('#container').children().first();
-/*let firstChildInsideSection = $('#container').children().eq(0);
-console.log(
-    firstChildInsideSection.nextAll('p')
-);
-
-$('ul li').slice(3, 8).css('color', 'yellow');*/
-
-
-/*
-console.log(
-    container.children('.bar').css('color', 'red')
-);*/
+    // li.appendTo('ul:first');
+    // $('ul').append(li);
+});
